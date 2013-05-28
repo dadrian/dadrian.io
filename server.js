83f8e67468b4874553ac17d32f324f3e4374f641
David Adrian <davidcadrian@gmail.com>
2013-05-28 19:21:42 -0400

    Working modular structure
    It's not the greatest - site still depends on blog. However, this is
    handled internally by the fact that index.js imports blog, then site
    (and calls app.use() and them in the correct order).
    
    Views are still served out of the top directory.

--- server.js
+++ server.js
@@ -1,93 +1,28 @@
 var util      = require('util'),
     express   = require('express'),
+    http      = require('http'),
     validator = require('express-validator')
     poet      = require('poet'),
     CONF      = require('config'),
     path      = require('path');
 
-var app   = express(),
-    blog  = poet(app);
+// Import private libraries
+var app    = express(),
+    routes = require('./lib/routes');
 
-blog.set({
-  posts: path.join(__dirname, CONF.poet.postdir), // Post directory
-  postsPerPage: 5,              // Posts per page in pagination
-  metaFormat: 'json',           // Meta format for post head matter
-  routes : {
-    post : '/blog/post/:post',
-    page : '/blog/page/:page',
-    tag : '/blog/tag/:tag',
-    category : 'blog/category/:category'
-  },
-  readMoreLink: function(post) {
-    var anchor = '<a href="' + post.url + '" title="Read more of ' + 
-        post.title + '"> ...read more</a>';
-    return '<p>' + anchor + '</p>';
-  },
-  readMoreTag: '<!--more-->'
-}).init(function(locals) {
-  locals.postList.forEach(function(post) {
-    //console.log(post);
-  });
-});
+// General global config of Express
+app.set('port', CONF.app.port);
+app.set('views', path.join(CONF.app.rootdir, 'views'));
+app.set('view engine', 'jade'); // I want to change this, probably ejs
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(CONF.app.rootdir, CONF.app.staticdir)));
 
-blog.postsPerPage = 5;
+// Set up the routes
+app.use(routes);
 
-app.configure(function() {
-  app.set('view engine', 'jade');
-  app.set('views', path.join(__dirname, 'views'));
-  app.use(express.static(path.join(__dirname, 'assets')));
-  app.use(validator);
-  app.use(blog.middleware());
-})
-
-
-app.get('/', function(req, res){
-  var posts = req.poet.getPosts(0, 2);
-  res.render('index', {pageTitle: 'David Adrian', posts: posts});
-});
-
-app.get('/blog', function(req, res) {
-  res.redirect('/blog/page/1');
-});
-
-app.get('/blog/page/:page', function(req, res) {
-  req.assert('page', 'Invalid Page Number').isInt().min(1);
-  var errors = req.validationErrors();
-  if (errors) {
-    res.send('500 Error ' + util.inspect(errors), 500);
-  }
-  var page      = req.params.page,
-      lastPost  = page * blog.postsPerPage,
-      firstPost = lastPost - blog.postsPerPage,
-      posts     = req.poet.getPosts(firstPost, lastPost);
-  console.log(page);
-  console.log(firstPost);
-  console.log(lastPost);
-  console.log(posts.length);
-  if (posts.length) {
-    res.render('blog', {
-      pageTitle: 'David Adrian | Blog Page ' + page,
-      posts: posts
-    });
-  } else {
-    res.send('404', 404);
-  }
-});
-
-app.get('/blog/post/:post', function(req, res) {
-  var post = req.poet.getPost(req.params.post);
-  if (post) {
-    res.render('post', {
-      pageTitle: 'Post',
-      post: post
-    });
-  }
-  res.send('404', 404);
-});
-
-app.get('/projects', function(req, res) {
-  res.render('projects', {pageTitle: 'David Adrian | Projects'});
-});
-
+// Start it up
 app.listen(CONF.app.port);
-console.log('Express started on port ' + CONF.app.port);
+console.log('Express server listening on port ' + CONF.app.port);
