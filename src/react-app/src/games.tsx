29424a78b2804c99a3f37ee9cca369ed7e55cae9
David Adrian <davidcadrian@gmail.com>
2025-04-20 11:08:42 -0400

    Use MUI x-data-grid for the table
    This library is overly heavy, but I guess it works.

--- src/react-app/src/games.tsx
+++ src/react-app/src/games.tsx
@@ -1,12 +1,84 @@
 import React from "react";
 import { aaa_platforms } from "./gen/aaa_platforms";
 import { yearly_categories } from "./gen/yearly_categories";
-import Table from "./table";
 
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, ReferenceLine } from 'recharts';
+
+import { createTheme } from '@mui/material/styles';
+import type {} from '@mui/x-data-grid/themeAugmentation';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+  palette: {
+    DataGrid: {
+      bg: 'transparent',
+      pinnedBg: 'transparent',
+      headerBg: 'transparent',
+    },
+  },
+});
 
 export const AAAPlatforms: React.FC = () => {
-  return <Table data={[...aaa_platforms]} />;
+  const aaa_platforms_columns: GridColDef[] = [
+    {
+      field: "name",
+      type: "string",
+      headerName: "Name",
+      align: 'left',
+      width: 150,
+    },
+    {
+      field: "generation",
+      type: "string",
+      headerName: "Generation",
+      headerAlign: 'right',
+      align: 'right',
+      width: 100,
+    },
+    {
+      field: "category",
+      type: "string",
+      headerName: "Category",
+      headerAlign: 'right',
+      align: 'right',
+      width: 160,
+    },
+    {
+      field: "slug",
+      type: "string",
+      headerName: "IGDB Slug",
+      headerAlign: 'right',
+      align: 'right',
+      width: 150,
+    },
+    {
+      field: "id",
+      type: "string",
+      headerName: "IGDB ID",
+      headerAlign: 'right',
+      align: 'right',
+    }
+
+  ];
+  const paginationModel = { page: 0, pageSize: 5 };
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+      <DataGrid
+        className="transparentGrid"
+        rows={[...aaa_platforms]}
+        columns={aaa_platforms_columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10, 100]}
+        sx={{
+          border: 0,
+          backgroundColor: 'transparent',
+         }}
+      />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
 };
 
 export const GameCountsByYear: React.FC = () => {
